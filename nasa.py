cc741b2be2dd9d3f825693c8b98b893b00cddfa3
lucass762 <lucasemanu292@gmail.com>
2026-06-30 16:33:16 -0300

    decimo primeiro commit

--- nasa.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-data = '2004-06-22' # Substitua por sua data de nascimento no formato AAAA-MM-DD
-
-resultado = requests.get('https://api.nasa.gov/planetary/apod?api_key=vnMl7ciifUgMrnGdGgLP1rdffr0kF1sqYhIhroxM&date={}'.format(data))
-print(resultado.status_code)
-print(resultado.json())
